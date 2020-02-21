16e69b90ae63a07c071038d7656c89cccaa50c2f
daniel mcnamara <danmac8407@gmail.com>
2020-02-21 11:26:38 -0600

    working on stretch

--- Answers.md
+++ Answers.md
@@ -2,10 +2,20 @@
 
 1. What is React JS and what problems does it try and solve? Support your answer with concepts introduced in class and from your personal research on the web.
 
+A.React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
+
 1. What does it mean to think in react?
+
+  A.React is, in my opinion, the way to build big, fast Web apps with JavaScript. One of the many great parts of React is how it makes you think about apps as you build them.
 
 1. Describe state.
 
+A. State is data that comprises a web app and the things that change that data(state)
+
 1. Describe props.
 
+ A.Props are read-only data. They help to pass information held on state inside one component to another component, we pass them as props.
+
 1. What are side effects, and how do you sync effects in a React component to state or prop changes?
+
+ A.A side effect is anything that affects something outside the scope of the function being executed. We use a hook
--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,10 +2,13 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import CharContainer from './components/CharContainer';
 import './App.css';
-
+import { Button } from "reactstrap";
 
 const App = () => {
-  const [data, setData] = useState([]);
+const [data, setData] = useState([]);
+const [url, setUrl] = useState("https://swapi.co/api/people/");
+const [next, setNext] = useState();
+const [prev, setPrev] = useState();
 
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -16,10 +19,11 @@ const App = () => {
 
   useEffect(() => {
     axios
-      .get('https://swapi.co/api/people/')
+      .get(url)
       .then(res => {
-        console.log(res.data.results);
         setData(res.data.results);
+        setNext(res.data.next);
+        setPrev(res.data.previous);
       })
       .catch(err => {
         console.log(err);
@@ -29,6 +33,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+
+      
+
       <CharContainer parentData={data} />
     </div>
   );
